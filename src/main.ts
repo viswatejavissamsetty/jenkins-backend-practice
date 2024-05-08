a92292ce20f4a1746430eb714ac8b58c87b435d7
viswatejavissamsetty <viswatejavissamsetty@gmail.com>
2024-05-08 09:52:28 +0530

    chore: Add console.log to check environment variables in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env);
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
