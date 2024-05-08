24b30d01783b89d9f648516cd6fc963d63bbf54e
viswatejavissamsetty <viswatejavissamsetty@gmail.com>
2024-05-08 11:02:08 +0530

    Temp message

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env);
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
